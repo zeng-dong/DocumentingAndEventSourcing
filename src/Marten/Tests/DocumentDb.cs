ca3b36d5ee9df0c17972b992e4c93f16e0936f71
Dong Zeng <dong@freshbyte.com>
2021-12-12 09:46:44 -0600

    async save and load test

--- src/Marten/Tests/DocumentDb.cs
+++ src/Marten/Tests/DocumentDb.cs
@@ -3,6 +3,7 @@ using Marten;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -52,6 +53,39 @@ namespace Tests
             using (var session = _theStore.QuerySession())
             {
                 var order2 = session.Load<Order>(order.Id);
+                order2.Should().NotBeNull();
+                order2.Should().NotBeSameAs(order);
+
+                order2.CustomerId.Should().Be(order.CustomerId);
+
+                _output.WriteLine(JsonConvert.SerializeObject(order2));
+            }
+        }
+
+        [Fact]
+        public async Task save_and_load_order_async()
+        {
+            var order = new Order
+            {
+                Priority = Priority.High,
+                CustomerId = "Kroger",
+                Details = new List<OrderDetail>()
+                {
+                    new OrderDetail { PartNumber = "Abc101", Number = 5 },
+                    new OrderDetail { PartNumber = "Apple", Number = 15 }
+                }
+            };
+
+            using (var session = _theStore.LightweightSession())
+            {
+                session.Store(order);
+                await session.SaveChangesAsync();
+            }
+
+            using (var session = _theStore.QuerySession())
+            {
+                var order2 = await session.LoadAsync<Order>(order.Id);
+
                 order2.Should().NotBeNull();
                 order2.Should().NotBeSameAs(order);
 
